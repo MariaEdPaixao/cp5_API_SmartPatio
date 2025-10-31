759128e7afe019bf1b8ebf2ccc4868e6c4270c78
MariaEdPaixao <mariaeduardaalvesdapaixao0807@gmail.com>
2025-10-31 09:39:40 -0300

    feat: value object de localização no carrapato

---
+++ Aplicacao/DTOs/Carrapato/CarrapatoMongoCriarDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao.DTOs.Carrapato
+{
+    public class CarrapatoMongoCriarDto
+    {
+        public string CodigoSerial { get; set; } = string.Empty;
+        public int IdPatio { get; set; }
+
+        // Campos opcionais só pro Mongo
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+    }
+}
